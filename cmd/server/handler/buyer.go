b5fb34b080b9bbf72a7fa570d8f67bd750916918
giuliago <giu_goliveira@hotmail.com>
2023-06-13 20:58:22 -0300

    swagger

--- cmd/server/handler/buyer.go
+++ cmd/server/handler/buyer.go
@@ -20,38 +20,61 @@ func NewBuyer(b buyer.Service) *Buyer {
 	}
 }
 
+// @Summary Get a buyer by ID
+// @Description Get a buyer by ID
+// @Tags buyers
+// @Param id path int true "Buyer ID"
+// @Success 200 {object} domain.Buyer
+// @Failure 400 {string} string "Invalid ID"
+// @Failure 404 {string} string "Buyer not found"
+// @Router /buyers/{id} [get]
 func (b *Buyer) Get() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
-			web.Error(c, http.StatusBadRequest, "invalid id")
+			web.Error(c, http.StatusBadRequest, "Invalid ID")
 			return
 		}
 		buyer, err := b.buyerService.Get(c, id)
 		if err != nil {
-			web.Error(c, http.StatusNotFound, "invalid id")
+			web.Error(c, http.StatusNotFound, "Buyer not found")
 			return
 		}
 		web.Success(c, http.StatusOK, buyer)
 	}
 }
 
+// @Summary Delete a buyer by ID
+// @Description Delete a buyer by ID
+// @Tags buyers
+// @Param id path int true "Buyer ID"
+// @Success 200 {string} string "Buyer deleted"
+// @Failure 400 {string} string "Invalid ID"
+// @Failure 404 {string} string "Buyer not found"
+// @Router /buyers/{id} [delete]
 func (b *Buyer) Delete() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
-			web.Error(c, http.StatusBadRequest, "invalid id")
+			web.Error(c, http.StatusBadRequest, "Invalid ID")
 			return
 		}
 		err = b.buyerService.Delete(c, id)
 		if err != nil {
-			web.Error(c, http.StatusNotFound, "invalid id")
+			web.Error(c, http.StatusNotFound, "Buyer not found")
 			return
 		}
-		web.Success(c, http.StatusOK, err)
+		web.Success(c, http.StatusOK, "Buyer deleted")
 	}
 }
 
+// @Summary Get all buyers
+// @Description Get all buyers
+// @Tags buyers
+// @Success 200 {array} domain.Buyer
+// @Failure 500 {string} string "Buyer not found"
+// @Failure 204 {string} string "No buyers found"
+// @Router /buyers [get]
 func (b *Buyer) GetAll() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		buyers, err := b.buyerService.GetAll(c)
@@ -67,39 +90,50 @@ func (b *Buyer) GetAll() gin.HandlerFunc {
 	}
 }
 
+// @Summary Create a new buyer
+// @Description Create a new buyer
+// @Tags buyers
+// @Accept json
+// @Param buyer body domain.BuyerCreate true "Buyer object"
+// @Success 201 {object} domain.Buyer
+// @Failure 422 {string} string "Buyer not created"
+// @Router /buyers [post]
 func (b *Buyer) Create() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var buyer domain.BuyerCreate
 		if err := c.ShouldBindJSON(&buyer); err != nil {
-			web.Error(c, http.StatusUnprocessableEntity, "buyer not created")
+			web.Error(c, http.StatusUnprocessableEntity, "Buyer not created")
 			return
 		}
 		buyerF, err := b.buyerService.Create(c.Request.Context(), buyer)
 		if err != nil {
-			web.Error(c, http.StatusUnprocessableEntity, "buyer not created")
+			web.Error(c, http.StatusUnprocessableEntity, "Buyer not created")
 			return
 		}
 		web.Response(c, http.StatusCreated, buyerF)
 	}
 }
 
+// @Summary Update a buyer by ID
+// @Description Update a buyer by ID
+// @Tags buyers
+// @Accept json
+// @Param id path int true "Buyer ID"
+// @Param buyer body domain.Buyer true "Buyer object"
+// @Success 200 {object} domain.Buyer
+// @Failure 400 {string} string "Invalid ID"
+// @Failure 404 {string} string "Buyer not updated"
+// @Router /buyers/{id} [put]
 func (b *Buyer) Update() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var buyer domain.Buyer
 		if err := c.ShouldBindJSON(&buyer); err != nil {
-			web.Error(c, http.StatusUnprocessableEntity, "buyer not created")
+			web.Error(c, http.StatusUnprocessableEntity, "Buyer not created")
 			return
 		}
-		/*cardID := c.Param(("card_number_id"))
-		fmt.Println(" ", cardID)
-		if cardID != "" {
-			fmt.Println("a ...any")
-			web.Error(c, http.StatusUnprocessableEntity, "buyer not created")
-			return
-		}*/
 		id, err := strconv.Atoi(c.Param("id"))
 		if err != nil {
-			web.Error(c, http.StatusBadRequest, "invalid id")
+			web.Error(c, http.StatusBadRequest, "Invalid ID")
 			return
 		}
 		buyerUpdated, err := b.buyerService.Update(c, buyer, id)
--- internal/domain/buyer.go
+++ internal/domain/buyer.go
@@ -1,5 +1,6 @@
 package domain
 
+// Buyer represents a buyer
 type Buyer struct {
 	ID           int    `json:"id"`
 	CardNumberID string `json:"card_number_id"`
@@ -7,9 +8,15 @@ type Buyer struct {
 	LastName     string `binding:"required" json:"last_name"`
 }
 
+// BuyerCreate represents the data for creating a buyer
 type BuyerCreate struct {
 	ID           int    `json:"id"`
 	CardNumberID string `binding:"required" json:"card_number_id"`
 	FirstName    string `binding:"required" json:"first_name"`
 	LastName     string `binding:"required" json:"last_name"`
+}
+
+// ErrorResponse represents an error response
+type ErrorResponse struct {
+	Message string `json:"message"`
 }
