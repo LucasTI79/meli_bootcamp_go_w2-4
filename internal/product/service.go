8502a8e786ba69c15a5a0d8e014eb6b3b4e48c84
Victor Giovannoni Vernalha <victor.vernalha@mercadolivre.com>
2023-06-20 12:45:45 -0300

    Fix same product code update causing 409

--- internal/product/service.go
+++ internal/product/service.go
@@ -94,13 +94,13 @@ func (s *service) Update(c context.Context, id int, updates UpdateDTO) (domain.P
 		return domain.Product{}, NewErrGeneric("could not fetch products")
 	}
 
-	if updates.Code.HasVal && !isUniqueProductCode(updates.Code.Val, ps) {
-		return domain.Product{}, NewErrInvalidProductCode(updates.Code.Val)
-	}
-
 	p, err := s.repo.Get(c, id)
 	if err != nil {
 		return domain.Product{}, NewErrNotFound(id)
+	}
+
+	if code, hasVal := updates.Code.Value(); hasVal && p.ProductCode != code && !isUniqueProductCode(code, ps) {
+		return domain.Product{}, NewErrInvalidProductCode(updates.Code.Val)
 	}
 
 	updated := applyUpdates(p, updates)
