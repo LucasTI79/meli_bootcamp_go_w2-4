5403f5be5c066a268732bca38a10eb603f230d78
lucasgandac <lucasgandac@hotmail.com>
2023-06-22 11:57:35 -0300

    Add getbyid service test

---
+++ internal/buyer/service_test.go
@@ -0,0 +1,121 @@
+package buyer_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/extmatperez/meli_bootcamp_go_w2-4/internal/buyer"
+	"github.com/extmatperez/meli_bootcamp_go_w2-4/internal/domain"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateBuyer(t *testing.T) {
+	t.Run("Create valid buyer", func(t *testing.T) {
+		repositoryMock := RepositoryMock{}
+		svc := buyer.NewService(&repositoryMock)
+
+		buyer := domain.BuyerCreate{
+			ID:           1,
+			CardNumberID: "123",
+			FirstName:    "nome",
+			LastName:     "sobrenome",
+		}
+
+		repositoryMock.On("Exists", mock.Anything, "123").Return(false)
+		repositoryMock.On("Save", mock.Anything, buyer).Return(1, nil)
+
+		received, err := svc.Create(context.TODO(), buyer)
+
+		assert.NoError(t, err)
+		assert.Equal(t, buyer, received)
+	})
+
+	t.Run("Create Buyer with conflict", func(t *testing.T) {
+		repositoryMock := RepositoryMock{}
+		svc := buyer.NewService(&repositoryMock)
+
+		buyerMock := domain.BuyerCreate{
+			ID:           1,
+			CardNumberID: "123",
+			FirstName:    "nome",
+			LastName:     "sobrenome",
+		}
+
+		repositoryMock.On("Exists", mock.Anything, "123").Return(true)
+
+		_, err := svc.Create(context.TODO(), buyerMock)
+
+		repositoryMock.AssertNumberOfCalls(t, "Save", 0)
+		assert.Equal(t, err.Error(), "buyer already exists")
+	})
+}
+
+func TestGetbyIDBuyer(t *testing.T) {
+	t.Run("Find by non existent Id", func(t *testing.T) {
+		repositoryMock := RepositoryMock{}
+		svc := buyer.NewService(&repositoryMock)
+
+		repositoryMock.On("Get", mock.Anything, 12).Return(domain.Buyer{}, errors.New("buyer not found"))
+
+		returned, err := svc.Get(context.TODO(), 12)
+
+		assert.Error(t, err)
+		assert.Equal(t, err.Error(), "buyer not found")
+		assert.Equal(t, domain.Buyer{}, returned)
+	})
+
+	t.Run("Find by existent Id", func(t *testing.T) {
+		repositoryMock := RepositoryMock{}
+		svc := buyer.NewService(&repositoryMock)
+		buyerMock := domain.BuyerCreate{
+			ID:           1,
+			CardNumberID: "123",
+			FirstName:    "nome",
+			LastName:     "sobrenome",
+		}
+
+		repositoryMock.On("Get", mock.Anything, 12).Return(buyerMock, nil)
+
+		returned, err := svc.Get(context.TODO(), 12)
+
+		assert.NoError(t, err)
+		assert.Equal(t, buyerMock, returned)
+	})
+
+}
+
+type RepositoryMock struct {
+	mock.Mock
+}
+
+func (r *RepositoryMock) GetAll(ctx context.Context) ([]domain.Buyer, error) {
+	args := r.Called(ctx)
+	return args.Get(0).([]domain.Buyer), args.Error(1)
+}
+
+func (r *RepositoryMock) Get(ctx context.Context, id int) (domain.Buyer, error) {
+	args := r.Called(ctx, id)
+	return args.Get(0).(domain.Buyer), args.Error(1)
+}
+
+func (r *RepositoryMock) Exists(ctx context.Context, cardNumberID string) bool {
+	args := r.Called(ctx, cardNumberID)
+	return args.Get(0).(bool)
+}
+
+func (r *RepositoryMock) Save(ctx context.Context, s domain.BuyerCreate) (int, error) {
+	args := r.Called(ctx, s)
+	return args.Get(0).(int), args.Error(1)
+}
+
+func (r *RepositoryMock) Update(ctx context.Context, s domain.Buyer) error {
+	args := r.Called(ctx, s)
+	return args.Error(1)
+}
+
+func (r *RepositoryMock) Delete(ctx context.Context, id int) error {
+	args := r.Called(ctx, id)
+	return args.Error(1)
+}
