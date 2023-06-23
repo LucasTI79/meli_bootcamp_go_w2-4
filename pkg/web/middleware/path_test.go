f9c9e9f545155c62dc43ba3823b0292f96fd294e
Victor Giovannoni Vernalha <victor.vernalha@mercadolivre.com>
2023-06-23 09:49:31 -0300

    Add handler retrieves param middleware test

--- pkg/web/middleware/path_test.go
+++ pkg/web/middleware/path_test.go
@@ -41,4 +41,22 @@ func TestIntPathParamValidator(t *testing.T) {
 
 		assert.Equal(t, http.StatusBadRequest, res.Code)
 	})
+	t.Run("Handler should be able to retrieve path param as int", func(t *testing.T) {
+		server := testutil.CreateServer()
+
+		intParam := middleware.IntPathParam()
+		code := 42
+		handler := func(ctx *gin.Context) {
+			givenCode := ctx.GetInt("code")
+			assert.Equal(t, code, givenCode)
+			web.Success(ctx, 200, nil)
+		}
+		server.GET("/:code", intParam, handler)
+
+		url := fmt.Sprintf("/%d", code)
+		req, res := testutil.MakeRequest(http.MethodGet, url, "")
+		server.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+	})
 }
