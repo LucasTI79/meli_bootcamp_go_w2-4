d00ab113ab436c6004a212972d3507d127026683
Victor Giovannoni Vernalha <victor.vernalha@mercadolivre.com>
2023-06-23 09:18:31 -0300

    Add body parse ok middleware test

---
+++ pkg/web/middleware/body_test.go
@@ -0,0 +1,38 @@
+package middleware_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/extmatperez/meli_bootcamp_go_w2-4/pkg/testutil"
+	"github.com/extmatperez/meli_bootcamp_go_w2-4/pkg/web"
+	"github.com/extmatperez/meli_bootcamp_go_w2-4/pkg/web/middleware"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBodyMapper(t *testing.T) {
+	t.Run("Should proceed to handler if body parsing succeeds", func(t *testing.T) {
+		server := testutil.CreateServer() // TODO: Remove testutil dependency
+
+		bodyMapper := middleware.Body[PrimitiveTypesBody]()
+		handler := func(ctx *gin.Context) { web.Success(ctx, 200, nil) }
+		server.POST("/", bodyMapper, handler)
+
+		body := map[string]any{
+			"id":     23,
+			"name":   "John Doe",
+			"height": 1.91,
+		}
+		req, res := testutil.MakeRequest(http.MethodPost, "/", body)
+		server.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+	})
+}
+
+type PrimitiveTypesBody struct {
+	ID     int     `binding:"required"`
+	Name   string  `binding:"required"`
+	Height float64 `binding:"required"`
+}
