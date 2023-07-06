0fa6823feb949f84592af260c6953bf5579d65c4
ThamyrisCabralArraes <thamyriscabral@hotmail.com>
2023-07-06 15:27:42 -0300

    add struct in the section for get report

--- internal/domain/section.go
+++ internal/domain/section.go
@@ -1,13 +1,19 @@
 package domain
 
 type Section struct {
-	ID                 int `json:"id"`
-	SectionNumber      int `json:"section_number"`
-	CurrentTemperature int `json:"current_temperature"`
-	MinimumTemperature int `json:"minimum_temperature"`
-	CurrentCapacity    int `json:"current_capacity"`
-	MinimumCapacity    int `json:"minimum_capacity"`
-	MaximumCapacity    int `json:"maximum_capacity"`
-	WarehouseID        int `json:"warehouse_id"`
-	ProductTypeID      int `json:"product_type_id"`
+	ID                 int     `json:"id"`
+	SectionNumber      int     `json:"section_number"`
+	CurrentTemperature float64 `json:"current_temperature"`
+	MinimumTemperature float64 `json:"minimum_temperature"`
+	CurrentCapacity    int     `json:"current_capacity"`
+	MinimumCapacity    int     `json:"minimum_capacity"`
+	MaximumCapacity    int     `json:"maximum_capacity"`
+	WarehouseID        int     `json:"warehouse_id"`
+	ProductTypeID      int     `json:"product_type_id"`
+}
+
+type GetOneData struct {
+	SectionId     int `json:"section_id"`
+	SectionNumber int `json:"section_number"`
+	ProductCount  int `json:"product_count"`
 }
